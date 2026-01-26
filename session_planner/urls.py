cb8d73db226b110f34cf90dc4619c9c0be4f91e9
hackneychap <samuelbern@gmail.com>
2026-01-26 13:27:29 +0000

    favicon

--- session_planner/urls.py
+++ session_planner/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import differentiated_plan_view,planner_page_view,add_workout_segment,recalculate_group_plan_view
+from .views import generate_plan_view,planner_page_view,add_workout_segment,recalculate_group_plan_view,add_repeat_block
 
 
 urlpatterns = [
     path('', planner_page_view, name='planner-page'),
-    path('generate-plan/',differentiated_plan_view,name='generate-plan' ),
+    path('generate-plan/',generate_plan_view,name='generate-plan' ),
     path('add-workout-segment/', add_workout_segment, name='add-workout-segment'),
     path('recalculate-plan/', recalculate_group_plan_view, name='recalculate-plan'),
+    path('add-repeat-block/', add_repeat_block, name='add-repeat-block'),
 ]
