e38a3ed5b5cabad153ee3a0d767f52e14725ca65
hackneychap <samuelbern@gmail.com>
2026-02-03 13:17:14 +0000

    resolve static files

--- speed_sessions/settings.py
+++ speed_sessions/settings.py
@@ -141,6 +141,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles' # Directory where collectstatic will gather files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
